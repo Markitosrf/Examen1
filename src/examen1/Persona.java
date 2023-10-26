75082e622dd1e1ef07354b84dcc5b26d53788595
MarcosRojas <mrojas40187@ufide.ac.cr>
2023-10-25 20:47:42 -0600

    se agrega el examen

---
+++ src/examen1/Persona.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen1;
+
+/**
+ *
+ * @author Marco
+ */
+public class Persona {
+    private String nombre;
+    private int cedula;
+    private int numero;
+    private double monto;
+    private String mes;
+    private int anio;
+    private int estado;
+    private int facturas;
+    
+    public Persona(String nombre,int cedula,int numero,double monto,String mes, int anio,int estado,int facturas){
+        this.nombre=nombre;
+        this.cedula=cedula;
+        this.numero=numero;
+        this.monto=monto;
+        this.mes=mes;
+        this.anio=anio;
+        this.estado=estado;
+        this.facturas=facturas;
+    }
+    public double calcular(){
+        for(int i=0;i<this.facturas;i++){
+            
+        }
+    }
+      
+}
+
+
+    
+    
+
